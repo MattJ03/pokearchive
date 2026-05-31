57d1b739b7f93f32cd4dcaf3fa5bfbd828782fca
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-06-01 00:43:29 +0100

    chore: setup finished

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from "../screens/Home.vue";
 
 const routes = [
-    { path: '/home', component: Home, name: Home },
+    { path: '/', component: Home, name: Home },
 ];
 
 const router = createRouter({
