ea78cdfdc2192345671b02bed70728afd3687908
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-06-01 00:07:34 +0100

    chore: set up the App.vue

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-view />
+</template>
+<script>
+export default {
+    name: "App",
+};
+</script>
+<style scoped>
+   body {
+       font-family: Arial, Helvetica, sans-serif;
+       margin: 0;
+       padding: 0;
+       background: #FAF9F6;
+   }
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,1 @@
-//
+import { createApp } from "vue";
