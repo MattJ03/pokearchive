56c8a0398cbb344e9d7c012e65491feb0f64ca38
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-06-01 21:43:27 +0100

    feat: added the inage wrapper

--- resources/js/components/PokeCard.vue
+++ resources/js/components/PokeCard.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="card-container">
-         <h1>hello</h1>
+         <div class="top-of-card">
+             <p class="poke-id">001</p>
+         </div>
+        <div class="img-wrapper">
+            <img class="poke-img" alt="pokemon image" />
+        </div>
     </div>
 
 </template>
@@ -9,8 +14,32 @@
 <style scoped>
 .card-container {
     display: flex;
+    flex-direction: column;
     border: 1px solid #ffffff;
-    height: 8dvh;
-    width: 15%;
+    height: 40dvh;
+    width: 17%;
+    border-radius: 14px;
+}
+.top-of-card {
+    display: flex;
+    flex-direction: row;
+     padding-top: 10px;
+}
+.poke-id {
+    color: #D3D3D3;
+    padding-left: 10px;
+}
+.img-wrapper {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10%;
+    width: 80%;
+    height: 35%;
+    background-color: #F45156;
+    border-radius: 14px;
+    opacity: 40%;
+}
+.poke-img {
+
 }
 </style>
